ff705a4a4cb69226adb16acb36127e1d891f909e
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2020-10-06 10:52:04 +0800

    Bug Fix
    TypeError: 'str' object is not callable
    You should `python clean.py`

--- index_com.py
+++ index_com.py
@@ -9,13 +9,13 @@ def head_name(str):
     file = open("index.html", "w")
     cur_log.execute('select * from Html1')
     for write_out in cur_log.fetchall():
-        file.write(str(write_out[1]))
+        file.write(write_out[1])
     file.close()
     # ====================
     file = open("index.html", "a")
     cur_log.execute('select * from Html2')
     for write_out in cur_log.fetchall():
-        file.write(str(write_out[1]))
+        file.write(write_out[1])
     file.close()
     # ====================
     conn_log.close()
