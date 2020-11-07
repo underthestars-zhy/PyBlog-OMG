986002d1a3bbff0de12a02716dd29efb7fcbaa32
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2020-11-07 11:50:50 +0800

    Test

--- index_com.py
+++ index_com.py
@@ -36,6 +36,10 @@ def main_name(str):
     mainame1 = '''\t\t\t\t\t<a href="index.html">'''+str+'''</a>\n'''
     cur_log.execute('''update Html1 set statement='{}' where id=33'''.format(mainame))
     cur_log.execute('''update Html1 set statement='{}' where id=42'''.format(mainame1))
+    mainame = '''\t\t\t\t\t<div class="nav-mobile-capital"><a href="index.html">''' + str + '''</a>\n'''
+    mainame1 = '''\t\t\t\t\t<a href="index.html">''' + str + '''</a>\n'''
+    cur_log.execute('''update Page1 set statement='{}' where id=33'''.format(mainame))
+    cur_log.execute('''update Page1 set statement='{}' where id=42'''.format(mainame1))
     conn_log.commit()
     # =====================
     file = open("index.html", "w")
@@ -64,7 +68,7 @@ def big_name(str):
     conn_log = sqlite3.connect("index.db")
     cur_log = conn_log.cursor()
     bigname='''\t\t\t\t\t\t<h1 class="banner-title">'''+str+'''</h1>\n'''
-    cur_log.execute('''update Html1 set statement='{}' where id=94'''.format(bigname))
+    cur_log.execute('''update Html2 set statement='{}' where id=36'''.format(bigname))
     conn_log.commit()
     # =====================
     file = open("index.html", "w")
@@ -93,7 +97,7 @@ def vice_name(str):
     conn_log = sqlite3.connect("index.db")
     cur_log = conn_log.cursor()
     vicename='''\t\t\t\t\t\t<p>'''+str+'''</p>\n'''
-    cur_log.execute('''update Html1 set statement='{}' where id=95'''.format(vicename))
+    cur_log.execute('''update Html2 set statement='{}' where id=37'''.format(vicename))
     conn_log.commit()
     # =====================
     file = open("index.html", "w")
@@ -123,6 +127,7 @@ def head_ico(str):
     cur_log = conn_log.cursor()
     ico='''\t<link rel="icon" href="'''+str+'''">\n'''
     cur_log.execute('''update Html1 set statement='{}' where id=11'''.format(ico))
+    cur_log.execute('''update Page1 set statement='{}' where id=11'''.format(ico))
     conn_log.commit()
     # =====================
     file = open("index.html", "w")
@@ -151,7 +156,7 @@ def main_img(str):
     conn_log = sqlite3.connect("index.db")
     cur_log = conn_log.cursor()
     img='''\t\t\t<img src="'''+str+'''" class="banner-img">\n'''
-    cur_log.execute('''update Html1 set statement='{}' where id=90'''.format(img))
+    cur_log.execute('''update Html2 set statement='{}' where id=32'''.format(img))
     conn_log.commit()
     # =====================
     file = open("index.html", "w")
