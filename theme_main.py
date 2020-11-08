2206d9ee252f7ecea0577d856d45d2a7bb53786a
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2020-11-08 09:19:42 +0800

    For Test

--- theme_main.py
+++ theme_main.py
@@ -1,5 +1,16 @@
 def theme_main ():
     import os
+    import sqlite3
+    try:
+        conn_log = sqlite3.connect("index.db")
+        cur_log = conn_log.cursor()
+        cur_log.execute('select * from ThemeDown')
+        for down_in in cur_log.fetchall():
+            if down_in[0] == 1:
+                return True
+        conn_log.close()
+    except:
+        pass
     try:
         os.system("rm main_txt.py")
         os.system("wget https://github.com/underthestars-zhy/PyBlog-OMG/raw/main/main_txt.py")
@@ -8,7 +19,12 @@ def theme_main ():
         os.system("rm index.py")
         os.system("wget https://github.com/underthestars-zhy/PyBlog-OMG/raw/main/index.py")
         os.system("clear")
-        return True
+        conn_log = sqlite3.connect("index.db")
+        cur_log = conn_log.cursor()
+        cur_log.execute('''Create table ThemeDown(tf int)''')
+        cur_log.execute('''insert into ThemeDown Values(1)''')
+        conn_log.commit()
+        conn_log.close()
     except:
         return False
 def theme_tf ():
